1c6667c8c16e0bd012b15c31dde4ea7a245403fd
ADSANDY18 <49060791+ADSANDY18@users.noreply.github.com>
2020-04-13 13:38:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+   
  
     <link href="https://fonts.googleapis.com/css?family=Lato|Nanum+Gothic:700|Raleway&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="StyleNeon.css">
